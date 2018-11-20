e6a59ad532f178bc63fbfafa41bab662ba6c3347
dimitraz <dimitrazuccarelli@gmail.com>
2018-11-20 09:05:17 +0000

    Subscribe to creation and deletion of songs

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
     <div v-for="song in songs" :key="song.id">
+      <div class="md-elevation-1">
       Song name: {{ song.name }} <br />
       Song desc: {{ song.description }}<br />
       Song artist: {{ song.artist }}<br />
       Song album: {{ song.album }}
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +27,25 @@ const ListSongs = id => {
     }`;
 };
 
+const NewSong = id => {
+  return `subscription get {
+    playlistUpdated(playlistId: "${id}") {
+      id
+      artist
+      name
+      genre
+    }
+  }`;
+};
+
+const DeletedSong = id => {
+  return `subscription delete {
+    onDeleteSong(playlistId: "${id}") {
+      id
+    }
+  }`;
+};
+
 export default {
   name: "Playlist",
   data() {
@@ -33,13 +54,31 @@ export default {
     };
   },
   async created() {
-    console.log(this.$route.params.id);
-    console.log(ListSongs(this.$route.params.id));
     const data = await API.graphql(
       graphqlOperation(ListSongs(this.$route.params.id))
     );
-    console.log(data);
     this.songs = data.data.listSongs.items;
+
+    // Subscribe to song create
+    const subscribeCreate = API.graphql(
+      graphqlOperation(NewSong(this.$route.params.id))
+    ).subscribe({
+      next: song => this.songs.push(song.value.data.playlistUpdated)
+    });
+
+    // Subscribe to song delete
+    const subscribeDelete = API.graphql(
+      graphqlOperation(DeletedSong(this.$route.params.id))
+    ).subscribe({
+      next: song => {
+        const songId = song.value.data.onDeleteSong.id;
+        this.songs.splice(this.songs.findIndex(i => i.id === songId), 1);
+      }
+    });
+
+    // Stop receiving data updates from the subscription
+    // subscribeCreate.unsubscribe();
+    // subscribeDelete.unsubscribe();
   }
 };
 </script>
